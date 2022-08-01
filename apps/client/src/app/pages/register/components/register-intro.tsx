91e0943e990e1bf99582d96233fc2578bb2f35d4
Jozef Sovcik <jozef.sovcik@gmail.com>
2022-08-01 17:37:27 +0200

    fixed wording

--- apps/client/src/app/pages/register/components/register-intro.tsx
+++ apps/client/src/app/pages/register/components/register-intro.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Text } from 'grommet';
+import { Box, Button, Paragraph, Text } from 'grommet';
 import { TeamFragmentFragment } from '../../../generated/graphql';
 
 interface RegisterIntroProps {
@@ -15,15 +15,14 @@ export function RegisterIntro(props: RegisterIntroProps) {
   }
   return (
     <Box gap="medium">
-      <Text>
+      <Paragraph>
         Začínate registráciu tímu <Text weight="bold">{team.name} </Text>
-        na turnaj.
-      </Text>
-      <Text>
+        na turnaj. <br />
+        <br />
         Registrácia má nieľkoľko krokov v rámci ktorých si vyberiete program, turnaj a určíte
-        fakturačnú a dodaciu adresu. Pred potvrdením registrácie budete môcť všetky údaje
-        skontrolovať a upraviť.
-      </Text>
+        fakturačnú a adresu na dodanie materiálov. Pred odoslaním požiadavky na registráciu budete
+        môcť všetky údaje skontrolovať a upraviť.
+      </Paragraph>
       <Box justify="between" direction="row">
         <Button label="Späť" onClick={prevStep} />
         <Button primary label="Pokračovať" onClick={nextStep} />
