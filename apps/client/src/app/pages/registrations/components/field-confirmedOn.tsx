e9e389a8a1bd6256baf8100cba07f598811665bb
Jozef Sovcik <jozef.sovcik@gmail.com>
2022-08-01 17:17:47 +0200

    regstration date handling

--- apps/client/src/app/pages/registrations/components/field-confirmedOn.tsx
+++ apps/client/src/app/pages/registrations/components/field-confirmedOn.tsx
@@ -1,38 +1,34 @@
 import { toUtcDateString } from '@teams2/dateutils';
-import React, { useCallback } from 'react';
+import React from 'react';
 import { useAppUser } from '../../../components/app-user/use-app-user';
 import { LabelValue } from '../../../components/label-value';
 import { useNotification } from '../../../components/notifications/notification-provider';
 import {
   Registration,
-  useRegistrationClearShippedMutation,
-  useRegistrationSetShippedMutation,
+  useRegistrationClearConfirmedMutation,
+  useRegistrationSetConfirmedMutation,
 } from '../../../generated/graphql';
 import { SetClearDate } from './set-clear-date';
 
-export const FieldShippedOn = (props: {
-  registration: Pick<Registration, 'id' | 'shippedOn'>;
+export const FieldConfirmedOn = (props: {
+  registration: Pick<Registration, 'id' | 'confirmedOn'>;
   readOnly: boolean;
 }) => {
   const { registration, readOnly } = props;
   const { isAdmin } = useAppUser();
   const { notify } = useNotification();
-  const onError = useCallback(
-    () => notify.error('Nepodarilo sa aktualizovať registráciu.'),
-    [notify]
-  );
-
-  const [setShipped] = useRegistrationSetShippedMutation({ onError });
-  const [clearShipped] = useRegistrationClearShippedMutation({ onError });
+  const onError = () => notify.error('Nepodarilo sa aktualizovať dátum potvrdenia registrácie.');
+  const [setConfirmed] = useRegistrationSetConfirmedMutation({ onError });
+  const [clearConfirmed] = useRegistrationClearConfirmedMutation({ onError });
 
   return (
-    <LabelValue label="Materiály odoslané">
+    <LabelValue label="Potvrdená">
       <SetClearDate
         canEdit={isAdmin() && !readOnly}
-        date={registration.shippedOn}
-        onClear={() => clearShipped({ variables: { id: registration.id } })}
+        date={registration.confirmedOn}
+        onClear={() => clearConfirmed({ variables: { id: registration.id } })}
         onSet={() =>
-          setShipped({
+          setConfirmed({
             variables: { id: registration.id, date: toUtcDateString(new Date()) ?? '' },
           })
         }
