731bb79a4d92efb85caf7a23c04a9df7247ea24d
Jozef Sovcik <jozef.sovcik@gmail.com>
2024-02-04 23:08:11 +0100

    fixing deployment

--- apps/client/src/app/app-config.ts
+++ apps/client/src/app/app-config.ts
@@ -1,9 +1,17 @@
-export function getAppConfig() {
-  return {
-    // NX_* variables need to be set during app build time
-    rootApiUrl: process.env.NX_API_URL ?? 'http://localhost:5000',
-    sfAPIUrl: process.env.NX_SF_API_URL ?? 'http://localhost:3000',
-  };
+declare global {
+  interface Window {
+    NX_API_URL?: string | null;
+    NX_SF_API_URL?: string | null;
+  }
 }
+
+export const getAppConfig = () => {
+  const config = {
+    apiUrl: window.NX_API_URL ?? 'http://localhost:5000',
+    sfAPIUrl: window.NX_SF_API_URL ?? 'http://localhost:3000',
+  };
+
+  return config;
+};
 
 export const appConfig = getAppConfig();
